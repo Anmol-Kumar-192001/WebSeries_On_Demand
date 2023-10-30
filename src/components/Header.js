f7d1df59660eb0f6467d61fd177164753359272b
Anmol Kumar <anmol.kr.2k17@gmail.com>
2023-10-31 00:27:11 +0530

    authenticate navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import userIcon from '../assets/userIcon.png'
-import { signOut } from 'firebase/auth';
+import React, { useEffect } from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
 
   const navigate = useNavigate();
+  const dispatch=useDispatch();
+  const user=useSelector(store=>store.user)
 
   const handleSignOut = () => {
-    signOut(auth).then(() => {
-      navigate('/')
-    }).catch((error) => {
+    signOut(auth).then(() => {})
+    .catch((error) => {
       navigate('/error')
     });
   }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
+        navigate('/browse');
+      } else {
+        dispatch(removeUser());
+        navigate('/')
+      }
+    });
+  }, [])
 
   return (
     <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
@@ -23,13 +38,13 @@ const Header = () => {
         src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
         alt='logo'
       />
-      <div className='flex p-0'>
+     {user && <div className='flex p-0'>
         <img
-          className='w-12 h-12'
+          className='w-12 h-12 rounded-full'
           alt='userIcon'
-          src={userIcon} />
+          src={user?.photoURL} />
         <button onClick={handleSignOut} className='font-bold text-white'>Sign Out</button>
-      </div>
+      </div>}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,17 @@
 import React, { useState, useRef } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/validate'
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
 
   const [isSignInForm, setIsSignInForm] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
 
-  const navigate=useNavigate();
+  const dispatch = useDispatch();
 
   const name = useRef(null)
   const email = useRef(null)
@@ -30,8 +31,15 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: name.current.value, photoURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+          }).then(() => {
+            const { uid, email, displayName, photoURL } = auth.currentUser;
+            dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
+          }).catch((error) => {
+            setErrorMessage(error?.message)
+          });
           console.log(user);
-          navigate('/browse')
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -47,7 +55,6 @@ const Login = () => {
         // Signed in 
         const user = userCredential.user;
         console.log(user);
-        navigate('/browse')
       })
       .catch((error) => {
         const errorCode = error.code;
