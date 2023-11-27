b097e592e0fe7ee7790b63021978fd794d6a775d
Anmol Kumar <anmol.kumar2@capriglobal.in>
2023-11-28 01:08:49 +0530

    made the app responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,14 +46,14 @@ const Header = () => {
   }, [])
 
   return (
-    <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+    <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between'>
       <img
-        className='w-44'
+        className='w-44 mx-auto md:mx-0'
         src={LOGO}
         alt='logo'
       />
       {user &&
-        <div className='flex p-2'>
+        <div className='flex p-2 justify-between'>
           {showGptSearch && <select className='p-2 m-2 bg-gray-900 text-white' onChange={handleLanguageChange}>
             {SUPPORTED_LANGUAGES.map((lang) => (
               <option key={lang.idenitifier} value={lang.idenitifier}>{lang.name}</option>
@@ -66,7 +66,7 @@ const Header = () => {
             {showGptSearch ? 'Home Page' : 'GPT Search'}
           </button>
           <img
-            className='w-12 h-12'
+            className='hidden md:block w-12 h-12'
             alt='userIcon'
             src={user?.photoURL} />
           <button onClick={handleSignOut} className='font-bold text-white'>Sign Out</button>
